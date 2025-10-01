422fea1b7206dd27d84d397e8f6b36cbecf34798
Griphcode <148160412+GriphcodeDev@users.noreply.github.com>
2025-10-01 21:37:43 +0200

    commit

--- src/randomnum.js
+++ src/randomnum.js
@@ -7,6 +7,8 @@ const OutputValue = document.getElementById("output");
 const ClearValue = document.getElementById("clear");
 
 
+const MainPage = document.getElementById("mainpage")
+
 SubmitValueInt.addEventListener("click", () => {
 
     OutputValue.innerHTML = Math.floor(Math.random() * InputValue.value);
@@ -25,4 +27,6 @@ SubmitValueBinary.addEventListener("click", () => {
 
 ClearValue.addEventListener("click", () => {
     OutputValue.innerHTML = "Empty";
+    InputValue.innerHTML = null;
+    
 });
